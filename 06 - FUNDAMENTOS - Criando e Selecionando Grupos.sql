c6f43662301354bbe1af24f750be5e5576091f06
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2021-12-06 19:52:34 -0300

    (ATUALIZANDO) 06 - FUNDAMENTOS - Criando e selecionando grupos

--- 06 - FUNDAMENTOS - Criando e Selecionando Grupos.sql
+++ 06 - FUNDAMENTOS - Criando e Selecionando Grupos.sql
@@ -1,22 +1,16 @@
 /*
- SEÇÃO 10 - AULA 2
-14. Criando e Selecionando Grupos
+Autor: Vicente T Freitas
+Descrição: Curso de Oracle SQL Especialista: do Básico ao Avançado
 
--- Criando Grupos utilizando a Cláusula GROUP BY
+-- SEÇÃO 10 - Aula 02: Criando e Selecionando Grupos
 
-  SELECT coluna, função_group(coluna)
-  FROM tabela
-  [WHERE condição]
-  [GROUP BY expressão_group_by]
-  [ORDER BY];
-
--- Sequencia Lógica
-
-    1.WHERE - Selecione as linhas a serem recuperadas;
-    2.GROUP BY - Formar os grupos;
-    3.HAVING - Selecione so grupos a serem recuperados;
-    4.Exibir colunas ou expressões do SELECT ordenando pelo critério definido no ORDER BY
+--  Sequencia Lógica
+     > 1.WHERE - Selecione as linhas a serem recuperadas;
+     > 2.GROUP BY - Formar os grupos;
+     > 3.HAVING - Selecione so grupos a serem recuperados;
+     > 4.Exibir colunas ou expressões do SELECT ordenando pelo critério definido no ORDER BY
 */
+
 -- Criando grupos utilizando a cláusula GROUP BY
 SELECT department_id, AVG(salary)
 FROM employees
@@ -25,10 +19,8 @@ ORDER BY department_id;
 
 /*
 -- Utilizando a cláusula GROUP BY
-
-   Se o SELECT utiliza grupos, então todas as colunas ou expressões na lista da
-   cláusula SELECT que não estão em uma função de groupo devem estar na cláusula
-   GROUP BY
+    > Se o SELECT utiliza grupos, então todas as colunas ou expressões na lista da
+    cláusula SELECT que não estão em uma função de groupo devem estar na cláusula GROUP BY
 */
 
 SELECT department_id, job_id, SUM(salary)
@@ -45,25 +37,27 @@ SELECT department_id, AVG(salary)
 FROM employees
 GROUP BY department_id;
 
-/* Consultas incorretas utilizando funções de grupo
-    Você não pode utilizar a cláusula WHERE para retringir grupos
-    Você não pode utilizar a Funções de grupo na cláusula WHERE
-    
-    OBS: Utilizar a cláusula HAVING para restringir grupos.
+/* 
+--  Consultas incorretas utilizando funções de grupo
+     > Você não pode utilizar a cláusula WHERE para retringir grupos
+     > Você não pode utilizar a Funções de grupo na cláusula WHERE
+     OBS: Utilizar a cláusula HAVING para restringir grupos.
 */
 SELECT department_id, MAX(salary)
 FROM employees
 WHERE MAX(salary) > 10000
 GROUP BY department_id;
 
-/*  Corrigindo consultas incorretas utilizando Funções de Grupo
+/*  
+--  Corrigindo consultas incorretas utilizando Funções de Grupo
     Restrigindo grupos utilizando a cláusula HAVING
         SELECT coluna, função_grupo
         FROM tabela
         [WHERE condição_linhas]
         [GROUP BY expressão_grupo]
         [HAVING condição_grupos]
-        [ORDER BY coluna]; */
+        [ORDER BY coluna]; 
+*/
 SELECT department_id, MAX(salary)
 FROM employees
 GROUP BY department_id
@@ -77,39 +71,11 @@ GROUP BY job_id
 HAVING SUM(salary) > 10000
 ORDER BY SUM(salary);
 
-/*  Aninhando funções de grupo
-        Exibindo a maior média de salário por departamento*/
+/*  
+--  Aninhando funções de grupo
+     > Exibindo a maior média de salário por departamento
+*/
 SELECT MAX(AVG(salary))
 FROM employees
 GROUP BY department_id;
-
-
- 
- 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+   
