ab95b70a621987824fdbebf2cee93884405ca4eb
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2021-12-07 18:06:20 -0300

    (ATUALIZANDO) 07 - FUNDAMENTOS - Exibindo dados a partir de múltiplas tabelas

--- 07 - FUNDAMENTOS - Exibindo dados a partir de Múltiplas Tabelas.sql
+++ 07 - FUNDAMENTOS - Exibindo dados a partir de Múltiplas Tabelas.sql
@@ -1,13 +1,15 @@
 /*
- SEÇÃO 11 - AULA 1
-15. Exibindo dados a partir de Múltiplas Tabelas
+Autor: Vicente T Freitas
+Descrição: Curso de Oracle SQL Especialista: do Básico ao Avançado
+
+-- SEÇÃO 11 - Aula 01: Exibindo dados a partir de Múltiplas Tabelas
 
 -- OBJETIVOS
-    Escrever comandos SELECT para acessar dados de mais de uma tabela utilizando os vários tipos de JOIN;
-    Visualizar dados que não correspondem a uma condição de JOIN utilizando OUTER JOIN;
-    Executar um JOIN de uma tabela com ela mesma utilizando um SELF JOIN;;
-    Entender o que é um produto cartesiano;
-    Gerar um produto cartesiano utilizando um CROSS JOIN.
+    > Escrever comandos SELECT para acessar dados de mais de uma tabela utilizando os vários tipos de JOIN;
+    > Visualizar dados que não correspondem a uma condição de JOIN utilizando OUTER JOIN;
+    > Executar um JOIN de uma tabela com ela mesma utilizando um SELF JOIN;;
+    > Entender o que é um produto cartesiano;
+    > Gerar um produto cartesiano utilizando um CROSS JOIN.
 
 --  Tipos de JOIN compatíveis com SQL ANSI 1999
         Natural JOIN:
@@ -21,8 +23,8 @@
         CROSS JOIN
     
 --  Qualificando nomes de colunas ambíguos
-        Utilize prefixos de tabelas para qualificar nomes de colunas que são iguais em multiplas tabelas;
-        Utilize preficos de tabela para otimizar a performace.
+     > Utilize prefixos de tabelas para qualificar nomes de colunas que são iguais em multiplas tabelas;
+     > Utilize preficos de tabela para otimizar a performace.
 */
 
 -- Utilizando prefixos de coluna com nomes de tabelas
@@ -32,12 +34,13 @@ SELECT
 FROM employees JOIN departments
     ON(employees.department_id = departments.department_id);
     
-/*  Qualificando nomes de colunas ambíguos
-        Ao invés de prefixos com o nome completo da tabela utilize Alias de Tabela;
-        Alias de Tabela podem ser nomes abreviados mantendo o código SQL menor
-        e utilizando menos memória;
-        Utilize Alias de Tabela para diferenciar colunas que possuem nomes idênticos,
-        mas residem em tabelas diferentes.  */
+/*  
+--  Qualificando nomes de colunas ambíguos
+     > Ao invés de prefixos com o nome completo da tabela utilize Alias de Tabela;
+     > Alias de Tabela podem ser nomes abreviados mantendo o código SQL menor
+       e utilizando menos memória;
+     > Utilize Alias de Tabela para diferenciar colunas que possuem nomes idênticos,
+       mas residem em tabelas diferentes.  */
 SELECT 
     e.employee_id, e.last_name, e.department_id,
     d.department_name
@@ -45,13 +48,15 @@ FROM
     employees e JOIN departments d
     ON(e.employee_id = d.department_id);
     
-/*  Criando Natural Joins
-        A cláusula NATURAL JOIN é baseada nem todas as colunas nas duas tabelas que
-        possuem o mesmo nome;
-        Seleciona as linhas a partir de duas tabelas que possuem valores iguais em todas
-        as colunas envolvidas na cláusula;
-        Se as colunas possuem o mesmo nome, mas possuem diferentes tipos de dados, 
-        um erro será retornado. */
+/*  
+--  Criando Natural Joins
+     > A cláusula NATURAL JOIN é baseada nem todas as colunas nas duas tabelas que
+       possuem o mesmo nome;
+     > Seleciona as linhas a partir de duas tabelas que possuem valores iguais em todas
+       as colunas envolvidas na cláusula;
+     > Se as colunas possuem o mesmo nome, mas possuem diferentes tipos de dados, 
+       um erro será retornado. 
+*/
  -- Utilizando NATURAL JOIN
 SELECT 
     department_id, department_name, location_id, city
@@ -71,14 +76,11 @@ FROM employees e
     JOIN departments d USING(department_id);
     
     
-/*  Criando JOIN com a cláusual ON
-        Utilize a cláusula ON para especificar coindições ou espeficifcar colunas para o JOIN;
-        A condição de JOIN é separada de outras coindições de pesquisa;
-        A cláusula ON torna o código mais simples e fácil de entender.
-        
-        SELECT tabela.coluna, tabela.coluna
-        FROM tabela
-        JOIN tabela ON(condição_join)
+/*  
+--  Criando JOIN com a cláusual ON
+     > Utilize a cláusula ON para especificar coindições ou espeficifcar colunas para o JOIN;
+     > A condição de JOIN é separada de outras coindições de pesquisa;
+     > A cláusula ON torna o código mais simples e fácil de entender.
 */
 SELECT
     e.employee_id, e.last_name, e.department_id, d.location_id
@@ -132,30 +134,4 @@ SELECT
 FROM employees empregado JOIN employees gerente
     ON (empregado.employee_id = gerente.employee_id)
 ORDER BY empregado.employee_id;
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
