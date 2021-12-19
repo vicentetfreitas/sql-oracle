380be90a27a0d1851cfee9179724a680476e26d9
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2021-12-19 09:48:31 -0300

    31.Diferenças entre as Visões com prefixo USER, ALL e DBA

---
+++ 31 - SQL AVANÇADO - Diferenças entre as Visões com prefixo USER ALL e DBA.sql
@@ -0,0 +1,180 @@
+-- 31.Diferenças entre as Visões com prefixo USER, ALL e DBA
+/*
+-- Diferenças entre as Visões com prefixo USER, ALL e DBA
+   > Tipos de prefixo:
+     - USER: inclui objetos do schema do usuário que estar conectado
+     - ALL: inclui objetos do schema do usuário que estar conectado e objetos que
+     o usuário possui privilégios de acesso
+     - DBA: inclui todos os objetos do banco de dados. Somente os DBA´s ou usuário
+     para os que possuem os privilégios necessário podem acessar
+*/
+
+-- Conectar como usuário HR
+-- Todos os objetos do usuário no schema 
+SELECT *
+FROM USER_OBJECTS;
+
+-- Todos os objectos que o usuário possui e objetos com privilégios
+SELECT *
+FROM ALL_OBJECTS;
+
+-- Conectar como usuário SYS
+SELECT *
+FROM DBA_OBJECTS
+WHERE OWNER = 'HR';
+
+-- Consultando informações sobre tabelas
+-- Conectar como usuário HR
+SELECT *
+FROM USER_TABLES;
+
+SELECT *
+FROM ALL_TABLES;
+
+-- Conectar como usuário SYS
+SELECT *
+FROM DBA_TABLES
+WHERE  OWNER = 'HR';
+
+-- Consultando informações sobre colunas de tabelas
+-- Conectar como usuário HR
+SELECT  *
+FROM USER_TAB_COLUMNS
+WHERE table_name = 'EMPLOYEES'
+ORDER BY column_id;
+
+SELECT *
+FROM ALL_TAB_COLUMNS
+WHERE OWNER = 'HR' AND
+      table_name = 'EMPLOYEES'
+ORDER BY column_id;
+
+-- Conectar como usuário SYS
+SELECT *
+FROM   DBA_TAB_COLUMNS
+WHERE  owner = 'HR'  AND 
+       table_name = 'EMPLOYEES' 
+ORDER BY column_id;
+
+-- Consultando informações sobre constraints de tabelas
+-- Conectar como usuário hr
+
+SELECT *
+FROM   USER_CONSTRAINTS
+WHERE  table_name = 'EMPLOYEES';
+
+SELECT *
+FROM   ALL_CONSTRAINTS
+WHERE  owner = 'HR'  AND
+       table_name = 'EMPLOYEES';
+
+
+-- Conectar como usuário SYS
+SELECT *
+FROM   DBA_CONSTRAINTS
+WHERE  owner = 'HR'  AND
+       table_name = 'EMPLOYEES';
+
+-- Consultando informações sobre colunas de uma constraint
+-- Conectar como usuário hr
+
+SELECT *
+FROM   USER_CONS_COLUMNS
+WHERE  table_name = 'EMPLOYEES'  AND
+       constraint_name = 'EMP_EMP_ID_PK'
+ORDER BY position;
+
+SELECT *
+FROM   ALL_CONS_COLUMNS
+WHERE  owner = 'HR' AND 
+       table_name = 'EMPLOYEES'  AND
+       constraint_name = 'EMP_EMP_ID_PK'
+ORDER BY position;
+
+-- Conectar como usuário SYS
+SELECT *
+FROM   DBA_CONS_COLUMNS
+WHERE  owner = 'HR' AND 
+       table_name = 'EMPLOYEES'  AND
+       constraint_name = 'EMP_EMP_ID_PK'
+ORDER BY position;
+
+-- Consultando informações sobre indices de tabelas
+-- Conectar como usuário hr
+
+SELECT *
+FROM   USER_INDEXES
+WHERE  table_name = 'EMPLOYEES';
+
+SELECT *
+FROM   ALL_INDEXES
+WHERE  owner = 'HR' AND 
+       table_name = 'EMPLOYEES';
+
+-- Conectar como usuário SYS
+SELECT *
+FROM   DBA_INDEXES
+WHERE  owner = 'HR' AND 
+       table_name = 'EMPLOYEES';
+
+-- Consultando informações sobre colunas que compõem indices de tabelas
+-- Conectar como usuário hr
+SELECT *
+FROM   USER_IND_COLUMNS
+WHERE  table_name = 'EMPLOYEES'  AND
+       index_name = 'EMP_EMP_ID_PK';
+
+SELECT *
+FROM   ALL_IND_COLUMNS
+WHERE  index_owner = 'HR' AND 
+       table_name = 'EMPLOYEES'  AND
+       index_name = 'EMP_EMP_ID_PK';
+
+-- Conectar como usuário SYS
+SELECT *
+FROM   DBA_IND_COLUMNS
+WHERE  index_owner = 'HR' AND 
+       table_name = 'EMPLOYEES'  AND
+       index_name = 'EMP_EMP_ID_PK';
+       
+-- Consultando informações sobre sequences
+-- Conectar como usuário hr
+SELECT *
+FROM   USER_SEQUENCES;
+
+SELECT *
+FROM   ALL_SEQUENCES
+WHERE  sequence_owner = 'HR';
+
+-- Conectar como usuário sys
+SELECT *
+FROM dba_sequences
+WHERE sequence_owner = 'HR';
+
+-- Consultando informações sobre views
+-- Conectar como usuário hr
+SELECT *
+FROM   USER_VIEWS;
+
+SELECT *
+FROM   ALL_VIEWS
+WHERE  owner = 'HR';
+
+-- Conectar como usuário sys
+SELECT *
+FROM dba_views
+WHERE owner = 'HR';
+
+-- Consultando informações sobre o banco de dados
+-- Conectar como usuário sys
+SELECT *
+FROM dba_users;
+
+SELECT *
+FROM dba_tablespaces;
+
+SELECT * 
+FROM dba_data_files;
+
+SELECT * 
+FROM dba_temp_files;
